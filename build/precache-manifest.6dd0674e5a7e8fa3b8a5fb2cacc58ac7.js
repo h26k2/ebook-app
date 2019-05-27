1b9c5b88c607aa884bbe70aa9c81d5ef8416323a
h26k2 <hasnain.karim262@outlook.com>
2019-05-28 00:11:59 +0500

    added build files

---
+++ build/precache-manifest.6dd0674e5a7e8fa3b8a5fb2cacc58ac7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6058aa862e58eee685efbc5a1a6f04dc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c575f499c0685cc26ca6",
+    "url": "/static/css/main.90402563.chunk.css"
+  },
+  {
+    "revision": "e40013701be12cbcafd2",
+    "url": "/static/js/2.d5aed670.chunk.js"
+  },
+  {
+    "revision": "c575f499c0685cc26ca6",
+    "url": "/static/js/main.4e4a8bac.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "99fcf2e4266ba0327b4eb62c1587e01f",
+    "url": "/static/media/compress-c1.99fcf2e4.jpg"
+  }
+]);
